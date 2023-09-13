3e3357cb8f58efb501656d10ef029b6bb4cdf823
Barry <S1186391@student.windesheim.nl>
2023-09-13 13:04:17 +0200

    index update

--- index.php
+++ index.php
@@ -1,35 +1,43 @@
 <?php
 
-$kites = ['Gin','Gaastra', 'Naish', 'kaas'];
-$names = ["Bert", "Henk", "Piet", "Irene", "Tim", "Marit"];
-sort($names);
-//die(var_dump($kites));
+//$kites = ['Gin','Gaastra', 'Naish', 'kaas'];
+$kites = [
+    'gin' => ['size'=>8,'lines'=>4 ],
+    'Gaastra' => ['size'=>12,'lines'=>5],
+    'Naish' => ['size'=>10,'lines'=>8]
+];
+foreach ($kites as $kite => $info){
+    $lines = $info['lines'];
+    $size = $info['size'];
+    echo "$kite has lines:$lines size:$size<br>";
+}
+$names = [
+    'Bert' =>  ['age' => 14, 'hobby' => ['gamen', 'slapen'], 'address' => "Winterkoning"],
+    'Piet' =>  ['age' => 18, 'hobby' => ['sporten', 'uitgaan'], 'address'=> "Haarlem"],
+    'Tim' =>   ['age' => 21, 'hobby' => ['pilsie poppen', 'gamen'], 'address' => "Nergens"],
+    'Irene' => ['age' => 19, 'hobby' => ['bordspellen'], 'address' => "Winterkoning"]
+];
 
-foreach ($names as $name){
-    echo"$name<br>";
+echo"<br><br><br>";
+
+foreach($names as $name=>$info){
+    $age = $info['age'];
+    echo "$name ($age) hobbies:  ";
+    for($i=0; $i<count($info['hobby']); $i++){
+        $hobby = $info['hobby'][$i];
+        echo$hobby;
+        if($i < count($info['hobby'])-1){
+            echo " & ";
+        }
+    }
+    $address = $info['address'];
+    echo "  woont: $address<br>";
 }
 
-echo "<br>";
-
-for($i=0; $i<Count($kites); $i++){
-    echo "$kites[$i]<br>";
-}
-//foreach ($kites as $kite){
-//   echo"$kite<br>";
-//}
 
 
-/**
- * Multidimensional Array
- */
 
-/**
- * for-loop
- */
 
-/**
- * foreach-loop
- */
 
 /**
  * Haal de presentatie van een variabele binnen
