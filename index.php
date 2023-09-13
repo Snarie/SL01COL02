a7570fc9f4ccd9a7dd83f89890a0282805690728
Barry <S1186391@student.windesheim.nl>
2023-09-13 10:51:18 +0200

    first

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 
+$kites = ['Gin','Gaastra', 'Naish', 'kaas'];
+$names = ["Bert", "Henk", "Piet", "Irene", "Tim", "Marit"];
+sort($names);
+//die(var_dump($kites));
 
-/**
- * Array
-*/
+foreach ($names as $name){
+    echo"$name<br>";
+}
+
+echo "<br>";
+
+for($i=0; $i<Count($kites); $i++){
+    echo "$kites[$i]<br>";
+}
+//foreach ($kites as $kite){
+//   echo"$kite<br>";
+//}
+
 
 /**
  * Multidimensional Array
